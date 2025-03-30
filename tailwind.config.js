ec2217c63a2775689e95f7e7f1e77f0a08c1aa47
Adel Kazem <adelkazem.1991@gmail.com>
2025-03-30 16:00:30 +0300

    productsss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        "./**/*.html",
+        "./**/*.js"
+    ],
+    theme: {
+        extend: {
+            // Your custom theme extensions
+        },
+    },
+    plugins: [
+        // Any plugins you want to use
+    ],
+}
